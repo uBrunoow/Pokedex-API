7ef7478acb80a4deb47d439d774b267db24d7c13
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-07-25 23:40:32 -0300

    Deploy Pokedex-API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,100 @@
-import Image from 'next/image'
+'use client'
+
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import PokedexImage from '../../public/pokedex.png'
 
 export default function Home() {
+
+  const [pokemon, setPokemon] = useState('');
+  const [searchPokemon, setSearchPokemon] = useState(1);
+
+  const pokemonImageRef = useRef<HTMLImageElement>(null);
+  const pokemonNameRef = useRef<HTMLSpanElement>(null);
+  const pokemonIdRef = useRef<HTMLSpanElement>(null);
+
+  const handlePokemon = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setPokemon(value.toLowerCase());
+  };
+
+  async function fetchPokemonData(pokemon: string) {
+    try {
+      const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+      if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        console.log(data)
+        return data;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+
+  
+  const renderPokemon = async (pokemon: string) => {
+    try {
+      const data = await fetchPokemonData(pokemon);
+      if (data) {
+        // Atualizar os estados para exibir os dados do Pokémon
+        setPokemon('');
+        setSearchPokemon(data.id);   
+
+        if (pokemonImageRef.current && pokemonNameRef.current && pokemonIdRef.current) {
+          pokemonImageRef.current.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+          pokemonNameRef.current.innerHTML = data.name;
+          pokemonIdRef.current.innerHTML = data.id
+        }
+
+      } else {
+        // Atualizar os estados para exibir uma mensagem de Pokémon não encontrado
+        setSearchPokemon(0);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    renderPokemon('1'); // O número '1' representa o primeiro Pokémon
+  }, []);
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <main className="bg-sky-500 h-screen w-full">
+        <div className=" flex items-center justify-center h-full w-full flex-col p-[15px]">
+          <Image src={PokedexImage} alt="pokedex" className=" w-full max-w-[426px] relative" />
+          <div className=" absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-full max-w-[426px] h-[638.48px]">
+            <Image src={''} alt="pokemon" className="w-[100px] h-[100px] absolute top-[50%] left-[50%] translate-x-[-70%] translate-y-[-100%]" ref={pokemonImageRef} />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <h1 className=" absolute bottom-[41%] left-[00%] w-[75%] flex items-center justify-end text-[#111] font-bold text-[20px]">
+                <span className=" mr-5" ref={pokemonIdRef}></span>
+                <span className="capitalize" ref={pokemonNameRef}> </span> 
+              </h1>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+              <form className=" absolute bottom-[25%] left-[11%] flex w-[70%] h-[50px]" onSubmit={(e) => { e.preventDefault(); renderPokemon(pokemon); }}>
+                <input
+                  type="search"
+                  className=" w-full p-[4%] outline-none border-x-[2px] border-y-[2px] border-black rounded-md"
+                  placeholder="Name or Number"
+                  required
+                  onChange={handlePokemon}
+                  value={pokemon}
+                />
+                <button type="submit"></button>
+              </form> 
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              <div className=" absolute bottom-10 left-10 w-[73%] flex gap-5 items-center justify-center">
+                <button className="w-[50%] p-[4%] border-x-[2px] border-y-[2px] border-black rounded-[5px] bg-[#ddd] font-bold btn-prev active:bg-[#777]" onClick={() => { if (searchPokemon > 1) renderPokemon((searchPokemon - 1).toString()); }}>Prev &lt;</button>
+                <button className="w-[50%] p-[4%] border-x-[2px] border-y-[2px] border-black rounded-[5px] bg-[#ddd] font-bold btn-next  active:bg-[#777]" onClick={() => renderPokemon((searchPokemon + 1).toString())}>Next &gt;</button>
+              </div>
+            </div>
+          </div>
+      </main>
+    </>
   )
 }
